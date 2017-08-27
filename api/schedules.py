c1c402257be3a78110d6a0aeb283fbd530d14304
106 <Segmentation-fault@yandex.ua>
2017-08-27 23:39:08 +0900

    単品のGETを追加

--- api/schedules.py
+++ api/schedules.py
@@ -53,8 +53,6 @@ class schedules_handler(APIHandler):
                 "id": i.id,
                 "time": i.next_run_time.isoformat(),
             }
-            print(i.kwargs)
-            #datas = dict(tuple([(k, v) for (k, v) in zip(["repeat", "level", "sound_id"], i.args)]))
             job.update(i.kwargs)
             jobs_lst.append(job)
         return jobs_lst
@@ -90,6 +88,7 @@ class schedules_handler(APIHandler):
         d = iso8601.parse_date(self.body["time"])
         if d < datetime.datetime.now(tz=TIMEZONE):
             api_assert(False, log_message="time must be later than present.")
+            return
 
         sound_id = ""
         if("sound_id" in self.body):
@@ -101,14 +100,39 @@ class schedules_handler(APIHandler):
         repeat = self.body["repeat"]
         if(repeat < 0):
             api_assert(False, log_message="repeat must be higer than zero.")
+            return
 
         waker().scheduler.add_job(waker.alarm_go_off, "date", run_date=d, kwargs={"repeat":repeat, "level":level, "sound_id":sound_id})
 
 class schedule_handler(schedules_handler):
-    __urls__ = ["/api/schedules/(?P<num>[0-9]+)/?$"]
+    __urls__ = ["/api/schedules/(?P<id>[0-9a-f]{32})/?$"]
 
-    def get(self, num):
-        try:
-            self.success(sche[int(num)])
-        except:
-            self.fail("No such data.")
+    @schema.validate(
+        output_schema = {
+            "type": "object",
+            "properties": {
+                "time":{"type": "string"},
+                "id": {"type": "string", "pattern":"[0-9a-f]{32}"},
+                "sound_id": {"type": "string", "pattern":"[0-9a-f]{32}"},
+                "level": {"enum":ALARM_LEVELS},
+                "repeat": {"type": "number"}
+            }
+        },
+        output_example = {
+            "time": "2017-08-26T00:00:00.000000+09:00",
+            "id": "586bc7d3dc2a4696bef5ea71ae5a0413",
+            "sound_id": "e0d2bb157aa4b43ce69ff3dccc2dd81d",
+            "level": "alarm",
+            "repeat": 0,
+        }
+    )
+    def get(self, id):
+        job = waker().scheduler.get_job(id)
+        if(not job):
+            api_assert(False, log_message="no such alarm.")
+        job_dict = {
+            "id": job.id,
+            "time": job.next_run_time.isoformat()
+        }
+        job_dict.update(job.kwargs)
+        return job_dict
