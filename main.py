009b6275e57da8508d1651bc80d301f7b4ba66b8
106 <Segmentation-fault@yandex.ua>
2017-11-01 02:49:50 +0900

    ws用のhandlerを追加

--- main.py
+++ main.py
@@ -4,8 +4,10 @@
 import os
 import json
 import logging
+import wsstream
 from datetime import datetime, timedelta
 from waker import waker
+from music_controller import music_controller
 from tornado.ioloop import IOLoop
 from tornado_json.routes import get_routes
 from tornado_json.application import Application
@@ -19,16 +21,14 @@ def main():
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
     wkr = waker()
-    #wkr.scheduler.add_job(waker.alarm_go_off, 'date', run_date=datetime.now()+timedelta(seconds=10), kwargs={"repeat":1, "level":"notify", "sound_id":"0"*32})
-    #wkr.scheduler.add_job(waker.alarm_go_off, kwargs={"repeat":1, "level":"notify", "sound_id":"0"*32})
 
     import api
     routes = get_routes(api)
+    routes.append(("/ws", wsstream.wsstream))
     print("Routes\n======\n\n" + json.dumps(
         [(url, repr(rh)) for url, rh in routes],
         indent=2)
     )
-
     app = Application(routes=routes, settings={})
     app.listen(8080)
 
