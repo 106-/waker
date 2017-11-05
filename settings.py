f81e751437ab3f9ed4649f6523439395cdd5dce7
106 <Segmentation-fault@yandex.ua>
2017-11-06 02:37:29 +0900

    mp3として認識するmimetypeを追加

--- settings.py
+++ settings.py
@@ -10,4 +10,4 @@ ALARM_LEVELS = ["alarm", "notify", "warning"]
 # 時計針の回し方、私次第かも!
 TIMEZONE = datetime.timezone( datetime.timedelta(hours=9) )
 
-SOUND_MIMETYPES = ["audio/mp3"]
+SOUND_MIMETYPES = ["audio/mp3", "audio/mpeg"]
